10d7c68ff78e7597e1ac08b3281394f8457c269a
Sandra <hi-sandra@hotmail.com>
2026-04-28 12:23:59 -0600

    buenas prácticas

--- tipo-de-dato.js
+++ tipo-de-dato.js
@@ -1,32 +1,32 @@
 // saludo
-console.log("Hola")
+console.log("Hola");
 
 //number
-console.log(typeof 42)
-console.log(typeof -666)
-console.log(typeof 0)
+console.log(typeof 42);
+console.log(typeof -666);
+console.log(typeof 0);
 
 //string
-let edad = 'veinticinco'
-console.log(typeof edad)
+let edad = 'veinticinco';
+console.log(typeof edad);
 
-let cantante = 'Andrea Bocelli'
-console.log(typeof cantante)
+let cantante = 'Andrea Bocelli';
+console.log(typeof cantante);
 
 //boolean
 
-let encendido = true
-let apagado = false
-console.log(typeof encendido)
+let encendido = true;
+let apagado = false;
+console.log(typeof encendido);
 
-let soleado = true
-let nublado = false
-console.log(typeof nublado)
+let soleado = true;
+let nublado = false;
+console.log(typeof nublado);
 
 //null
-let cumpleaños = null
-console.log(typeof cumpleaños)
+let cumpleaños = null;
+console.log(typeof cumpleaños);
 
 //undefined
-let apariencia
-console.log(typeof apariencia)
+let apariencia;
+console.log(typeof apariencia);
