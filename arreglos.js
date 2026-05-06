a8e0ac75b7c902382d831d842a873777c7e5dd2e
Sandra <hi-sandra@hotmail.com>
2026-05-05 19:56:35 -0600

    conteo

--- arreglos.js
+++ arreglos.js
@@ -2,3 +2,9 @@
 
 let frutas = ['platano', 'coco', 'fresa', 'melon', 'limon', 'naranja', 'mango', 'papaya', 'aguacate', 'durazno','cereza', 'frambuesa', 'ciruela', 'mandarina', 'kiwi', 'mamey', 'piña', 'mora']
 
+let conteo = {
+    neutras: 0,
+    dulces: 0,
+    semiacidas: 0,
+    acidas: 0
+};
