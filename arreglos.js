1349261bd19589e670e27d922e26f6ee1fbef6d8
Sandra <hi-sandra@hotmail.com>
2026-05-05 19:55:24 -0600

    arreglo de frutas

---
+++ arreglos.js
@@ -0,0 +1,4 @@
+// Array de frutas
+
+let frutas = ['platano', 'coco', 'fresa', 'melon', 'limon', 'naranja', 'mango', 'papaya', 'aguacate', 'durazno','cereza', 'frambuesa', 'ciruela', 'mandarina', 'kiwi', 'mamey', 'piña', 'mora']
+
