684eea78d5cc9cffc568f90a1eeab2a344da45da
Sandra <hi-sandra@hotmail.com>
2026-05-08 14:09:58 -0600

    lista de libros

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// sistema de libros
+
+//array de libros
+let LibrosLeidos = [
+    'Cuentos de Eva Luna',
+    'Boulevard',
+    'Obras completas de Sigmund Freud',
+    'El mundo de Sofia',
+    'Compañeros de la noche',
+    'Asi fue',
+    'Entrevista con el vampiro',
+    'Los juegos del hambre'
+];
